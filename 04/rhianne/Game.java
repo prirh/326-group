25468d759304632c9a2520e2fa922ecd8a387800
Josiah <josiahnelson1@gmail.com>
2017-03-27 21:37:55 +1300

    made Game/Move/Player classes don't know what they will do though'

---
+++ 04/rhianne/Game.java
@@ -0,0 +1,23 @@
+package etude04;
+import java.util.*;
+/**
+ * Game.java
+ * COSC326 Etude 4
+ * Peanuts and Pretzels
+ *
+ * @author Rhianne Price
+ * February 2017
+ **/
+public class Game {
+
+  private int peanutsLeft, pretzelsLeft;
+  private Move[] rules;
+
+  public Game(int peanuts, int pretzels, String rules) {
+    int rulesCount = rules.split(System.getProperty("line.separator")).length;
+
+    rules = new Move[rulesCount];
+    
+    pretzelsLeft = pretzels;
+    peanutsLeft = peanuts;
+  }
---
+++ 04/rhianne/Move.java
@@ -0,0 +1,19 @@
+package etude04;
+import java.util.*;
+/**
+ * Move.java
+ * COSC326 Etude 4
+ * Peanuts and Pretzels
+ *
+ * @author Rhianne Price
+ * February 2017
+ **/
+public class Move {
+
+  int[] peanuts;
+  int[] pretzels;
+
+  public Move(String move) {
+    peanutsLeft = peanuts;
+  }
+}
