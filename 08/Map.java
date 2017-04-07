b0cec5707ffd59cb90897c1d6670f98863cf4867
Rhianne <price.rhianne@gmail.com>
2017-04-07 12:29:15 +1200

    add constructor to map class

--- 08/Map.java
+++ 08/Map.java
@@ -4,10 +4,32 @@ import java.util.*;
 
 public class Map {
     ArrayList<Point> points;
-    ArrayList<double> distances;
+    ArrayList<Double> distances;
+    final int NUMBER_OF_POINTS;
 
     public Map(ArrayList<String> points) {
-        
+        Scanner scanner;
+        NUMBER_OF_POINTS = points.length;
+        distances = new ArrayList<Double>();
+
+        for(int i = 0; i < NUMBER_OF_POINTS; i++) {
+            scanner = new Scanner(points[i]);
+            this.points.add(new Point(scanner.nextDouble(), scanner.nextDouble()));
+        }
+
+        for(i = 0; i < NUMBER_OF_POINTS; i++) {
+            for(int j = 0; j < numberOfPoints; j++) {
+                this.points[i].addNeighbour(this.points[j]);
+            }
+            distances.add(this.points[i].furthestNeighbour());
+        }
     }
 
+    public double getMaxRange(){
+        return Collections.min(distances);
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
