2603631d445d48377ea4474b9eb5ef78db441817
Rhianne <price.rhianne@gmail.com>
2017-04-07 12:53:36 +1200

    access points working

--- 08/Map.java
+++ 08/Map.java
@@ -9,27 +9,24 @@ public class Map {
 
     public Map(ArrayList<String> points) {
         Scanner scanner;
-        NUMBER_OF_POINTS = points.length;
+        NUMBER_OF_POINTS = points.size();
         distances = new ArrayList<Double>();
+        this.points = new ArrayList<Point>();
 
         for(int i = 0; i < NUMBER_OF_POINTS; i++) {
-            scanner = new Scanner(points[i]);
+            scanner = new Scanner(points.get(i));
             this.points.add(new Point(scanner.nextDouble(), scanner.nextDouble()));
         }
 
-        for(i = 0; i < NUMBER_OF_POINTS; i++) {
-            for(int j = 0; j < numberOfPoints; j++) {
-                this.points[i].addNeighbour(this.points[j]);
+        for(int i = 0; i < NUMBER_OF_POINTS; i++) {
+            for(int j = 0; j < NUMBER_OF_POINTS; j++) {
+                this.points.get(i).addNeighbour(this.points.get(j));
             }
-            distances.add(this.points[i].furthestNeighbour());
+            distances.add(this.points.get(i).furthestNeighbour());
         }
     }
 
     public double getMaxRange(){
         return Collections.min(distances);
-    }
-
-    public static void main(String[] args) {
-        
     }
 }
