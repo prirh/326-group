a186f7d4cf652f03e7c88b7f1034a7ea69e412e4
Zachary Gardner <garza181@student.otago.ac.nz>
2017-05-16 17:16:10 +1200

    final touches will be done tommorow, outputs need formatting

--- etude3/ss/Arith2.java
+++ etude3/ss/Arith2.java
@@ -57,116 +57,9 @@ public class Arith2{
             
             
             // if Left to right maths, do the following.
-            if(splittedRes[1].charAt(0) == 'L'){
-                System.out.println("here1");
-                /*outer loop determines the combination being attempted.
-                  this is determined in a fashion similar to binary via if statements*/
-                for(int i =0; i<max&& found ==0;i++){
-                    total = inputs.get(0);
-                    output = "L "+total;
-                    currPattern="";
-                    if(i<preHalf){
-                        /* inner loop goes through each gap in numbers and gives it an operation*/
-                        for(int x=0; x<(inputs.size()-1)/2;x++){
-                            String temp =ss.get(x);
-                            Double preInfo =  new Double(Math.pow(2,x));
-                            int info = preInfo.intValue();
-                            /// if statements determining operation.
-                            if(x==0 && i%2==0){
-                                currPattern+="+";
-                                output +=" + " +inputs.get(x+1);   
-                                total+= inputs.get(x+1);
-                            }
-                            else if((i/info)%2==0){
-                                currPattern+="+";
-                                output +=" + " +inputs.get(x+1);
-                                total+= inputs.get(x+1);
-                            }
-                            else if((i/info)%2==1){
-                                currPattern+="*";
-                                output +=" * " +inputs.get(x+1);
-                                total*= inputs.get(x+1);
-                            }
-                            else{
-                                currPattern+="*";
-                                output +=" * " +inputs.get(x+1);
-                                total*= inputs.get(x+1);
-                            }
-                            // System.out.println(output);
-                        }
-                        ss.add(currPattern);
-                        halfTot.add(total);
-                    }else{
-                        
-                        
-                    }
-                    /* if total is found output the equation if not print impossible.*/
-                    if(total == Integer.valueOf(splittedRes[0])){
-                        System.out.println(output);
-                                found =1;
-                    }
-                }
-                if(found ==0){
-                    System.out.println(" L impossible");
-                }
-            }
+                       
             
-            
-            else if(splittedRes[1].charAt(0) == '?'){
-                System.out.println("here2");
-                for(int i =0; i<max &&found==0 ;i++){
-                    total = inputs.get(0);
-                    used.clear();
-                    output ="N " + total;
-                    /* inner loop goes through each gap and gives it an
-                       operation*/
-                    for(int x=0; x<inputs.size()-1;x++){
-
-                        /* rather tahn prefrom the operations dirrectly, a list
-                           of operations in order is made to be read from at
-                           the time of execution*/
-                        if(((i >>x)&1)==1){
-                            output +=" * " +inputs.get(x+1);
-                            used.add('*');
-                        }else{
-                            output +=" + " +inputs.get(x+1);
-                            used.add('+');
-                        }   
-                        
-                    }
-                    /*inputs are saved as the execution rewrites them and
-                      then they can restored for the next combination to
-                      be tried*/
-                    Results.clear();
-                    Results.addAll(inputs);
-                    /*the execution of the equation, starts by multiplying
-                      first and removing the multiplied numbers and opertaion,
-                      replacing it with the result in order to do normal maths*/
-                    while(used.contains('*')){
-                        inputs.set(used.indexOf('*'), inputs.get(used.indexOf('*')) * inputs.get(used.indexOf('*')+1));
-                        inputs.remove(used.indexOf('*')+1);
-                        used.remove(used.indexOf('*'));
-                    }
-                    total = inputs.get(0);
-                    for(int x=0; x<used.size();x++){
-                        total += inputs.get(x+1);
-                    }
-                    /* if total is found print the equation, if not print impossible*/
-                    if(total == Integer.valueOf(splittedRes[0])){
-                        System.out.println(output);
-                        found=1;
-                    }
-                    inputs.clear();
-                    inputs.addAll(Results);
-                }
-                if(found==0){
-                    System.out.println(" N impossible");
-                }
-                
-            }	
-            
-            
-            else if(splittedRes[1].charAt(0) == 'c'){
+            if(splittedRes[1].charAt(0) == 'N'){
                 System.out.println("here3");
                 long[] nums = new long[inputs.size()];
                 nums2 = new long[inputs.size()];
@@ -177,91 +70,51 @@ public class Arith2{
 
                 Arrays.sort(nums2);
                     
-                if(treeSolv(nums,nums.length-1,nums[0],nums[0],0,0,Integer.valueOf(splittedRes[0]),"")==0){
-                    System.out.println("notFound");
+                if(treeSolv(nums,nums.length-1,nums[0],nums[0],0,0,
+                            Integer.valueOf(splittedRes[0]),"")==0){
+                    System.out.println("Impossible N");
                 }
             }
+ 	    else if(splittedRes[1].charAt(0) == 'L'){
+                System.out.println("here3");
+                long[] nums = new long[inputs.size()];
+                nums2 = new long[inputs.size()];
+                for(int i=0;i<inputs.size();i++){
+                    nums[i] = inputs.get(i);
+                    nums2[i] = inputs.get(i);
+                }
+
+                Arrays.sort(nums2);
+                    
+                if(treeSolvL(nums,nums.length-1,nums[0],
+                            Integer.valueOf(splittedRes[0]),"")==0){
+                    System.out.println("Impossible L");
+                }
+            }
+
             //if normal maths do the following.
-            else{
-                System.out.println("here4");
-                /*outter loop determines the combination being attempted in
-                  a binary fashion via if statements*/
-                for(int i =0; i<max &&found==0 ;i++){
-                    total = inputs.get(0);
-                    used.clear();
-                    output ="N " + total;
-                    /* inner loop goes through each gap and gives it an
-                       operation*/
-                    for(int x=0; x<inputs.size()-1;x++){
-                        
-                        Double preInfo =  new Double(Math.pow(2,x));
-                        int info = preInfo.intValue();
-                        /* rather tahn prefrom the operations dirrectly, a list
-                           of operations in order is made to be read from at
-                           the time of execution*/
-                        if(x==0 && i%2==0){
-                            output +=" + " +inputs.get(x+1);
-                            used.add('+');
-                        }
-                        else if((i/info)%2==0){
-                            output +=" + " +inputs.get(x+1);
-                            used.add('+');
-                        }
-                        else if((i/info)%2==1){
-                            output +=" * " +inputs.get(x+1);
-                            used.add('*');
-                        }
-                        else{
-                            output +=" * " +inputs.get(x+1);
-                            used.add('*');
-                        }
-                        
-                    }
-                    /*inputs are saved as the execution rewrites them and
-                      then they can restored for the next combination to
-                      be tried*/
-                    Results.clear();
-                    Results.addAll(inputs);
-                    /*the execution of the equation, starts by multiplying
-                      first and removing the multiplied numbers and opertaion,
-                      replacing it with the result in order to do normal maths*/
-                    while(used.contains('*')){
-                        inputs.set(used.indexOf('*'), inputs.get(used.indexOf('*')) * inputs.get(used.indexOf('*')+1));
-                        inputs.remove(used.indexOf('*')+1);
-                        used.remove(used.indexOf('*'));
-                    }
-                    total = inputs.get(0);
-                    for(int x=0; x<used.size();x++){
-                        total += inputs.get(x+1);
-                    }
-                    /* if total is found print the equation, if not print impossible*/
-                    if(total == Integer.valueOf(splittedRes[0])){
-                        System.out.println(output);
-                        found=1;
-                    }
-                    inputs.clear();
-                    inputs.addAll(Results);
-                }
-                if(found==0){
-                    System.out.println(" N impossible");
-                }
-            }
+            
+                    
         }
-    
     }
 
 
-
-    public static int treeSolv(long[] nums,int i, long preNum, long prePlus, long preTimes, long total, long answer,String pattern){
+    public static int treeSolv(long[] nums,int i, long preNum, 
+		long prePlus, long preTimes, long total, long answer,String pattern){
         //System.out.println("doing" + test++);
         if(nums2[0]>=1 && i==0 && answer >total && pattern.indexOf('+')==1){
             //System.out.println(pattern + " " + answer + " : " + total);
             //System.out.println("Impossible");
+	    System.out.println("Impossible N");
             System.exit(0);
         }
         if(total ==answer && i ==0){
             // System.out.println(pattern + " " + answer + " : " + total);
             //System.out.println("Found");
+	    for(int x=0;x<nums.length-1;x++){
+                System.out.print(nums[x]+" "+pattern.charAt(x));
+            }
+            System.out.println(nums[nums.length-1]);
             return 1;
         }   
         else if(i==0){
@@ -298,5 +151,51 @@ public class Arith2{
         
         return 0;
     }
+
+    public static int treeSolvL(long[] nums,int i, long total, long answer,String pattern){
+        //System.out.println("doing" + test++);
+        if(nums2[0]>=1 && i==0 && answer >total && pattern.indexOf('+')==1){
+            //System.out.println(pattern + " " + answer + " : " + total);
+            System.out.println("Impossible L");
+            System.exit(0);
+        }
+        if(total ==answer && i ==0){
+            // System.out.println(pattern + " " + answer + " : " + total);
+            //System.out.println("Found");
+            for(int x=0;x<nums.length-1;x++){
+                System.out.print(nums[x]+" "+pattern.charAt(x));
+            }
+            System.out.println(nums[nums.length-1]);
+            return 1;
+        }   
+        else if(i==0){
+            //System.out.println(pattern + " " + answer + " : " + total);
+            //System.out.println(pattern + ": " + total);
+            return 0;
+        }
+        long num =nums[nums.length-i];
+        if(nums2[0] >=1 && total >answer){
+            // System.out.println(pattern + " " + answer + " : " + total);
+            return 0;
+            
+            //System.out.pattern
+        }
+        
+        
+        //System.out.println(i + ": " + preNum +": " + prePlus+": " + preTimes+": " + total+": " + answer);
+        String pat = pattern+ "+";
+        String pat2 = pattern +"*";
+            
+            if(treeSolvL(nums,i-1,total*num,answer,pat2)==1){
+                return 1;
+            }
+        
+        if(treeSolvL(nums,i-1,total+num,answer,pat)==1){
+            return 1; 
+        }
+        
+        return 0;
+    }
+
 }
 
